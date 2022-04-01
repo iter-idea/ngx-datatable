86401919bfc5dbf6a4fce57d62528a679597ecac
Matteo Carbone <mc@matteocarbone.com>
2022-04-01 11:43:30 +0200

    add dist

---
+++ dist/swimlane/ngx-datatable/lib/utils/selection.d.ts
@@ -0,0 +1,8 @@
+export declare function selectRows(selected: any[], row: any, comparefn: any): any[];
+export declare function selectRowsBetween(
+  selected: any[],
+  rows: any[],
+  index: number,
+  prevIndex: number,
+  comparefn: any
+): any[];
